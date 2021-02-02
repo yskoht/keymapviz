67287d0b8a279759568a6fb9455c25b1870732cf
Yusuke Ohta <34795067+yskoht@users.noreply.github.com>
2021-02-03 00:40:15 +0900

    Remove unused keyword (#39)

--- keymapviz/keyboards/kyria/__init__.py
+++ keymapviz/keyboards/kyria/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-keymap_keyword = '(?:LAYOUT|LAYOUT_kc|LAYOUT_split_2x6_8_5)'
+keymap_keyword = '(?:LAYOUT)'
 layout_editor_json = 'keyboards/kyria/layout_editor.json'
 
 ascii_art = '''
