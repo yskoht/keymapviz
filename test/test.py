423bd66f9c3edd38babcbf918389440dca4c17ed
Robb Enzmann <robbenzmann@gmail.com>
2022-02-27 17:25:07 -0500

    add default layout to the test set

--- test/test.py
+++ test/test.py
@@ -226,7 +226,21 @@ exe('diff -u expect/replace_lets_split_keymap_fancy.c input/replace_lets_split_k
 exe('keymapviz -k lets_split ./input/custom_legends_lets_split_keymap.c -c ./input/custom_legends_config.properties -o custom_legends_lets_split.c')
 exe('diff -u expect/custom_legends_lets_split.c custom_legends_lets_split.c')
 
-# planck (grid)
+# planck (default/grid)
+exe('keymapviz ./qmk_firmware/keyboards/planck/keymaps/default/keymap.c -o planck_default.c')
+exe('keymapviz ./qmk_firmware/keyboards/planck/keymaps/default/keymap.c -o planck_default_fancy.c -t fancy')
+exe('keymapviz ./qmk_firmware/keyboards/planck/keymaps/default/keymap.c -o planck_default_{}.json -t json')
+
+exe('diff -u expect/planck_grid.c       planck_default.c')
+exe('diff -u expect/planck_grid_fancy.c planck_default_fancy.c')
+exe('diff -u expect/planck_grid_0.json  planck_default_0.json')
+exe('diff -u expect/planck_grid_1.json  planck_default_1.json')
+exe('diff -u expect/planck_grid_2.json  planck_default_2.json')
+exe('diff -u expect/planck_grid_3.json  planck_default_3.json')
+exe('diff -u expect/planck_grid_4.json  planck_default_4.json')
+exe('diff -u expect/planck_grid_5.json  planck_default_5.json')
+
+
 exe('keymapviz ./qmk_firmware/keyboards/planck/keymaps/default/keymap.c -o planck_grid.c -l grid')
 exe('keymapviz ./qmk_firmware/keyboards/planck/keymaps/default/keymap.c -o planck_grid_fancy.c -t fancy -l grid')
 exe('keymapviz ./qmk_firmware/keyboards/planck/keymaps/default/keymap.c -o planck_grid_{}.json -t json -l grid')
